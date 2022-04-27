a4a5d0eb5dd7d8cf2148a97cdf3d51e8bd561c68
Christine Maynard <christine.ri.maynard@gmail.com>
2022-04-26 22:59:22 -0400

    First commit, alas it works

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return render_template('index.html')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
