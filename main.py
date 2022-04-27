43125925bc34a825f107b7c02cf9965d7992e36d
Christine Maynard <christine.ri.maynard@gmail.com>
2022-04-26 23:55:31 -0400

    package application and test local development run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from portfolio_app_v1 import app
+from flask import  render_template
 
 @app.route('/')
 def test():
@@ -9,8 +8,6 @@ def test():
 @app.route('/')
 def home():
     return render_template('home.html')
-
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ portfolio_app_v1/routes.py
@@ -0,0 +1,10 @@
+# from flask import  render_template
+# from portfolio_app_v1 import app
+
+# @app.route('/')
+# def test():
+#     return render_template('index.html')
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
